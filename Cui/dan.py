d29aed5cb33a21366847ba78958cd061bfe524ee
dimdym <dimdym@gmail.com>
2019-04-07 11:16:21 -0500

    got a basic dan to work using functional api

--- Cui/dan.py
+++ Cui/dan.py
@@ -19,21 +19,24 @@ import sys
 sys.path.append('../Lib/')
 sys.dont_write_bytecode = True
 import configparser
+
 from sklearn.metrics import f1_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.model_selection import train_test_split
-import keras as k
+
+from keras import Input
 from keras.utils.np_utils import to_categorical
 from keras.optimizers import RMSprop
 from keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential
+from keras.models import Model
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers import GlobalAveragePooling1D
 from keras.layers.embeddings import Embedding
 from keras.models import load_model
 from keras.callbacks import Callback
-import dataset, word2vec, callback
+
+import dataset
 
 # ignore sklearn warnings
 def warn(*args, **kwargs):
@@ -41,110 +44,58 @@ def warn(*args, **kwargs):
 import warnings
 warnings.warn = warn
 
-RESULTS_FILE = 'Model/results.txt'
-MODEL_FILE = 'Model/model.h5'
-
-def print_config(cfg):
-  """Print configuration settings"""
-
-  print('train:', cfg.get('data', 'train'))
-  if cfg.has_option('data', 'embed'):
-    print('embeddings:', cfg.get('data', 'embed'))
-  print('test_size', cfg.getfloat('args', 'test_size'))
-  print('batch:', cfg.get('dan', 'batch'))
-  print('epochs:', cfg.get('dan', 'epochs'))
-  print('embdims:', cfg.get('dan', 'embdims'))
-  print('hidden:', cfg.get('dan', 'hidden'))
-  print('learnrt:', cfg.get('dan', 'learnrt'))
-
-def get_model(cfg, init_vectors, num_of_features):
+def get_model(vocabulary_size, max_seq_len):
   """Model definition"""
 
-  model = Sequential()
-  model.add(Embedding(input_dim=num_of_features,
-                      output_dim=cfg.getint('dan', 'embdims'),
-                      input_length=maxlen,
-                      trainable=True,
-                      weights=init_vectors,
-                      name='EL'))
-  model.add(GlobalAveragePooling1D(name='AL'))
+  # input tensor not including batch size
+  input_tensor = Input(shape=(max_seq_len,))
 
-  model.add(Dense(cfg.getint('dan', 'hidden'), name='HL'))
-  model.add(Activation(cfg.get('dan', 'activation')))
+  x = Embedding(
+    input_dim=vocabulary_size,
+    output_dim=300,
+    input_length=max_seq_len)(input_tensor)
 
-  model.add(Dropout(cfg.getfloat('dan', 'dropout')))
+  x = GlobalAveragePooling1D(name='AL')(x)
 
-  model.add(Dense(classes))
-  model.add(Activation('sigmoid'))
+  x = Dense(512, activation='relu', name='HL')(x)
+
+  x = Dropout(0.25)(x)
+
+  output_tensor = Dense(1, activation='sigmoid')(x)
+
+  model = Model(input_tensor, output_tensor)
 
   model.summary()
+
   return model
 
 if __name__ == "__main__":
 
   cfg = configparser.ConfigParser()
   cfg.read(sys.argv[1])
-  print_config(cfg)
 
   base = os.environ['DATA_ROOT']
   train_dir = os.path.join(base, cfg.get('data', 'train'))
-  code_file = os.path.join(base, cfg.get('data', 'codes'))
 
-  dataset = dataset.DatasetProvider(
-    train_dir,
-    code_file,
-    cfg.getint('args', 'min_token_freq'),
-    cfg.getint('args', 'max_tokens_in_file'),
-    cfg.getint('args', 'min_examples_per_code'))
-  x, y = dataset.load()
+  dp = dataset.DatasetProvider(train_dir)
+  x, y = dp.load()
+  print('x shape:', x.shape)
+  print('y shape:', y.shape)
+
   train_x, val_x, train_y, val_y = train_test_split(
-    x,
-    y,
+    x, y,
     test_size=cfg.getfloat('args', 'test_size'))
-  maxlen = max([len(seq) for seq in train_x])
 
-  init_vectors = None
-  if cfg.has_option('data', 'embed'):
-    embed_file = os.path.join(base, cfg.get('data', 'embed'))
-    w2v = word2vec.Model(embed_file)
-    init_vectors = [w2v.select_vectors(dataset.token2int)]
-
-  # turn x into numpy array among other things
-  classes = len(dataset.code2int)
-  train_x = pad_sequences(train_x, maxlen=maxlen)
-  val_x = pad_sequences(val_x, maxlen=maxlen)
-  train_y = np.array(train_y)
-  val_y = np.array(val_y)
-
-  print('train_x shape:', train_x.shape)
-  print('train_y shape:', train_y.shape)
-  print('val_x shape:', val_x.shape)
-  print('val_y shape:', val_y.shape)
-  print('number of features:', len(dataset.token2int))
-  print('number of labels:', len(dataset.code2int))
-
-  if cfg.has_option('dan', 'optimizer'):
-    optimizer = cfg.get('dan', 'optimizer')
-  else:
-    optimizer = RMSprop(lr=cfg.getfloat('dan', 'learnrt'))
-
-  model = get_model(cfg, init_vectors, len(dataset.token2int))
+  model = get_model(len(dp.tokenizer.word_index)+1, x.shape[1])
   model.compile(loss='binary_crossentropy',
-                optimizer=optimizer,
+                optimizer='rmsprop',
                 metrics=['accuracy'])
   model.fit(train_x,
             train_y,
-            callbacks=[callback.Metrics()] if val_x.shape[0]>0 else None,
-            validation_data=(val_x, val_y) if val_x.shape[0]>0 else None,
+            validation_data=(val_x, val_y),
             epochs=cfg.getint('dan', 'epochs'),
             batch_size=cfg.getint('dan', 'batch'),
             validation_split=0.0)
-
-  model.save(MODEL_FILE)
-
-  # do we need to evaluate?
-  if cfg.getfloat('args', 'test_size') == 0:
-    exit()
 
   # probability for each class; (test size, num of classes)
   distribution = model.predict(val_x)
@@ -161,10 +112,3 @@ if __name__ == "__main__":
   p = precision_score(val_y, distribution, average='micro')
   r = recall_score(val_y, distribution, average='micro')
   print("micro: p: %.3f - r: %.3f - f1: %.3f" % (p, r, f1))
-
-  outf1 = open(RESULTS_FILE, 'w')
-  int2code = dict((value, key) for key, value in list(dataset.code2int.items()))
-  f1_scores = f1_score(val_y, distribution, average=None)
-  outf1.write("%s|%s\n" % ('macro', f1))
-  for index, f1 in enumerate(f1_scores):
-    outf1.write("%s|%s\n" % (int2code[index], f1))
--- Cui/dataset.py
+++ Cui/dataset.py
@@ -2,13 +2,13 @@
 
 import sys
 sys.dont_write_bytecode = True
-import os, shutil, random
+import os, shutil, random, numpy
 from configparser import ConfigParser
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 
 MODEL_DIR = 'Model/'
-TRAIN_SIZE = 0.80
+TRAIN_SIZE = 0.50
 
 class DatasetProvider:
   """Make x and y from raw data"""
@@ -16,13 +16,25 @@ class DatasetProvider:
   def __init__(self, corpus_path):
     """Constructor"""
 
+    self.corpus_path = corpus_path
+    self.tokenizer = Tokenizer(oov_token='oov_token')
+
+    # prepare model directory
+    if os.path.isdir(MODEL_DIR):
+      shutil.rmtree(MODEL_DIR)
+    os.mkdir(MODEL_DIR)
+
+  def load(self):
+    """Make x and y"""
+
     x = []
     y = []
-    for file in os.listdir(corpus_path):
 
-      path = os.path.join(corpus_path, file)
+    for file in os.listdir(self.corpus_path)[:50]:
+      path = os.path.join(self.corpus_path, file)
       text = open(path).read().split()
       unique = list(set(text))
+
       random.shuffle(unique)
 
       train_examples = round(len(unique) * TRAIN_SIZE)
@@ -30,78 +42,16 @@ class DatasetProvider:
       x.append(' '.join(unique[:train_examples]))
       y.append(' '.join(unique[train_examples:]))
 
-    tokenizer = Tokenizer()
-    tokenizer.fit_on_texts(x)
-    x = tokenizer.texts_to_sequences(x)
-    y = tokenizer.texts_to_sequences(y)
+    self.tokenizer.fit_on_texts(x)
+    x = self.tokenizer.texts_to_sequences(x)
+    y = self.tokenizer.texts_to_sequences(y)
 
     x = pad_sequences(x)
     y = pad_sequences(y)
 
-    print(x[234])
-    print('unique tokens:', len(tokenizer.word_index))
-    print('shape:', x.shape)
+    y = numpy.random.randint(0, 2, size=x.shape[0])
 
-
-
-    if os.path.isdir(MODEL_DIR):
-      print('removing old model directory...')
-      shutil.rmtree(MODEL_DIR)
-    os.mkdir(MODEL_DIR)
-
-  def load(self,
-           maxlen=float('inf'),
-           tokens_as_set=True):
-    """Convert examples into lists of indices"""
-
-    codes = []    # each example has multiple codes
-    examples = [] # int sequence represents each example
-
-    for file in os.listdir(self.corpus_path):
-      file_ngram_list = None
-      if self.use_cuis == True:
-        file_ngram_list = self.read_cuis(file)
-      else:
-        file_ngram_list = self.read_tokens(file)
-      if file_ngram_list == None:
-        continue # file too long
-
-      # make code vector for this example
-      subj_id = file.split('.')[0]
-      if subj_id not in self.subj2codes:
-        continue # subject was present once with no code
-      if len(self.subj2codes[subj_id]) == 0:
-        continue # shouldn't happen
-
-      code_vec = [0] * len(self.code2int)
-      for icd9_category in self.subj2codes[subj_id]:
-        if icd9_category in self.code2int:
-          # this icd9 has enough examples
-          code_vec[self.code2int[icd9_category]] = 1
-
-      if sum(code_vec) == 0:
-        continue # all rare codes for this file
-
-      codes.append(code_vec)
-
-      # represent this example as a list of ints
-      example = []
-
-      if tokens_as_set:
-        file_ngram_list = set(file_ngram_list)
-
-      for token in file_ngram_list:
-        if token in self.token2int:
-          example.append(self.token2int[token])
-        else:
-          example.append(self.token2int['oov_word'])
-
-      if len(example) > maxlen:
-        example = example[0:maxlen]
-
-      examples.append(example)
-
-    return examples, codes
+    return x, y
 
 if __name__ == "__main__":
 
@@ -110,5 +60,5 @@ if __name__ == "__main__":
   base = os.environ['DATA_ROOT']
   train_dir = os.path.join(base, cfg.get('data', 'train'))
 
-  dataset = DatasetProvider(train_dir)
-  # x, y = dataset.load()
+  dat_prov = DatasetProvider(train_dir)
+  x, y = dat_prov.load()
