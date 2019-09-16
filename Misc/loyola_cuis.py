b8efe77cfc64a734ecd0b30458b38cd5c38505ec
dimdym <dimdym@gmail.com>
2019-09-16 12:05:25 -0500

    now only selecting notes of a specific type (e.g. 'disch summaries')

--- Misc/loyola_cuis.py
+++ Misc/loyola_cuis.py
@@ -3,23 +3,35 @@
 import sys
 sys.dont_write_bytecode = True
 
-codes_file = '/Users/Dima/Temp/rxnorm.codes'
-output_dir = '/Users/Dima/Temp/Text/'
+import configparser
+
+# CUI file header:
+# RptID|RptDesc|MRN|Hsp_Account_ID|CodeDomain|Cui|Tui|PreferredText|Polarity|Count
 
 def parse():
   """Parse cui file"""
 
-  for line in open(codes_file):
+  cui_file = cfg.get('args', 'cui_file')
+  output_dir = cfg.get('args', 'output_dir')
+  note_type = cfg.get('args', 'note_type')
+
+  for line in open(cui_file):
     elements = line.strip().split('|')
     report_id = elements[0]
+    report_type = elements[1]
     cui = elements[5]
     count = elements[9]
 
-    out = open('%s%s.txt' % (output_dir, report_id), 'a')
+    if report_type != note_type:
+      continue
 
+    out = open('%s%s.txt' % (output_dir, report_id), 'a')
     for _ in range(int(count)):
       out.write(cui + ' ')
 
 if __name__ == "__main__":
+
+  cfg = configparser.ConfigParser(allow_no_value=True)
+  cfg.read(sys.argv[1])
 
   parse()
