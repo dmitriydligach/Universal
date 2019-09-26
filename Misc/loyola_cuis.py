6b5cd3df945cae8893cfa830630223f2173d1910
dimdym <dimdym@gmail.com>
2019-09-26 15:26:19 -0500

    updated cui extraction to work with different note types and multiple files

--- Misc/loyola_cuis.py
+++ Misc/loyola_cuis.py
@@ -8,12 +8,10 @@ import configparser
 # CUI file header:
 # RptID|RptDesc|MRN|Hsp_Account_ID|CodeDomain|Cui|Tui|PreferredText|Polarity|Count
 
-def parse():
+def parse(cui_file, note_prefix, out_dir):
   """Parse cui file"""
 
-  cui_file = cfg.get('args', 'cui_file')
-  output_dir = cfg.get('args', 'output_dir')
-  note_type = cfg.get('args', 'note_type')
+  print('searching %s prefix in %s...' % (note_prefix, cui_file))
 
   for line in open(cui_file):
     elements = line.strip().split('|')
@@ -22,16 +20,26 @@ def parse():
     cui = elements[5]
     count = elements[9]
 
-    if report_type != note_type:
-      continue
+    if report_type.startswith(note_prefix):
+      out = open('%s%s.txt' % (out_dir, report_id), 'a')
+      for _ in range(int(count)):
+        out.write(cui + ' ')
 
-    out = open('%s%s.txt' % (output_dir, report_id), 'a')
-    for _ in range(int(count)):
-      out.write(cui + ' ')
+def main():
+  """Main driver"""
+
+  root_dir = cfg.get('args', 'root_dir')
+  out_dir = cfg.get('args', 'out_dir')
+  note_prefixes = cfg.get('args', 'note_prefixes')
+  cui_files = cfg.get('args', 'cui_files')
+
+  for cui_file in cui_files.split('|'):
+    for note_prefix in note_prefixes.split('|'):
+      parse(root_dir + cui_file, note_prefix, root_dir + out_dir)
 
 if __name__ == "__main__":
 
   cfg = configparser.ConfigParser(allow_no_value=True)
   cfg.read(sys.argv[1])
 
-  parse()
+  main()
