62f6ba92a820f207988adfbe45a07e72d44b0c32
dimdym <dimdym@gmail.com>
2019-10-17 10:15:04 -0500

    finalized split (using 0.2 for dev set now)

--- Misc/split.py
+++ Misc/split.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
-import sys, random, glob, shutil
+import sys, random, glob, shutil, os
 
-source_dir = '/home/dima/Temp/Train/'
-dest_dir = '/home/dima/Temp/Dev/'
+source_dir = 'MimicIII/Encounters/Cuis/Train/'
+dest_dir = 'MimicIII/Encounters/Cuis/Dev/'
 
-def select_and_move(split=0.15):
+base = os.environ['DATA_ROOT']
+source_dir = os.path.join(base, source_dir)
+dest_dir = os.path.join(base, dest_dir)
+
+def select_and_move(split=0.20):
   """Pick random files and move to another dir"""
 
   all = glob.glob(source_dir + '*.txt')
   sample = random.sample(all, int(len(all) * split))
-  print('moving %d files...' % len(sample))
+  print('source dir:', source_dir)
+  print('dest dir:', dest_dir)
+  print('moving {} files...'.format(len(sample)))
 
   for item in sample:
     shutil.move(item, dest_dir)
