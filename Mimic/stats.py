f5091a109596e023157d95fea03b3109490a6cd2
Dima <ddligach@luc.edu>
2020-10-20 10:31:37 -0500

    added a function to count unique cuis mimic

--- Mimic/stats.py
+++ Mimic/stats.py
@@ -4,10 +4,28 @@ import sys
 sys.dont_write_bytecode = True
 import glob, numpy
 
-def main():
+cui_files = '/Users/Dima/Work/Data/MimicIII/Encounters/Cuis/All/*.txt'
+
+def unique_tokens():
   """Count tokens in files"""
 
-  cui_files = '/Users/Dima/Work/Data/MimicIII/Encounters/Cuis/All/*.txt'
+  counts = []
+  for file_path in glob.glob(cui_files):
+
+    file_as_string = open(file_path).read()
+    n_tokens = len(set(file_as_string.split()))
+    counts.append(n_tokens)
+
+  print('mean:', numpy.mean(counts))
+  print('median:', numpy.median(counts))
+  print('std:', numpy.std(counts))
+  print('min:', numpy.min(counts))
+  print('max:', numpy.max(counts))
+
+  print(sorted(counts))
+
+def all_tokens():
+  """Count tokens in files"""
 
   counts = []
   for file_path in glob.glob(cui_files):
@@ -22,8 +40,6 @@ def main():
   print('min:', numpy.min(counts))
   print('max:', numpy.max(counts))
 
-
-
 if __name__ == "__main__":
 
-  main()
+  unique_tokens()
